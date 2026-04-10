1d6b215b332a04beeb2b4625eae21a9a7db76480
ejwood003 <ejwood@byu.edu>
2026-04-09 18:16:17 -0600

    fixed dotnet build issue with google

--- backend/EbanHaven.Api/Controllers/AuthController.cs
+++ backend/EbanHaven.Api/Controllers/AuthController.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Authentication.Google;
 
 namespace EbanHaven.Api.Controllers;
 
